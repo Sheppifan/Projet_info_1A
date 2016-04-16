d5ffbd039c77c4988f055fc14cc1f1787314b4e6
Sheppifan <caroline.celie34@gmail.com>
2016-04-16 15:09:02 +0200

    Added files via upload

---
+++ graphe.c
@@ -0,0 +1,46 @@
+#include "graphe.h"
+#include "liste.h"
+#include <stdio.h>
+
+
+/// Fonctions de laure
+/*Graphe nouveau_graphe(unsigned int nX,unsigned int nA)
+
+
+void affiche_graphe(Graphe g)
+
+
+void detruit_graphe(Graphe g)
+
+
+void graphe_ecrit_nX(Graphe g, unsigned int nX)
+
+
+void graphe_ecrit_nA(Graphe g, unsigned int nA)
+
+
+unsigned int graphe_lit_nX(Graphe g)
+
+
+unsigned int graphe_lit_nA(Graphe g)*/
+
+///Fonctions de Caroline
+
+/*double graphe_lit_poids(Graphe g, unsigned int u)
+
+void graphe_ecrit_poids(Grapheg, unsigned int u, double val)
+
+void graphe_ecrit_poids_arc(Graphe g, unsigned int u, unsigned int v, double val)
+
+double graphe_lit_poids_arc(Graphe g, unsigned int u, unsigned int v)*/
+
+
+///fonctions Haut niveau
+
+/*Graphe lit_graphe(char* fichier)
+
+void graphe_ajoute_arc(Graphe g, unsigned int u, unsigned int v, double val)
+
+double graphe_pcc(Graphe g, unsigned int u, unsigned int v)
+*/
+
---
+++ graphe.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "liste.h"
+#include <stdlib.h>
+
+#ifndef _Graphe
+#define _Graphe
+
+typedef struct {
+    unsigned char* nom_station[50];
+    unsigned int nbligne;
+    unsigned int nX;
+    unsigned int nA;
+    double poids_noeud;
+    Liste* arc;
+    }  * Graphe
+
+Graphe nouveau_graphe(unsigned int nX,unsigned int nA);
+void affiche_graphe(Graphe g);
+void detruit_graphe(Graphe g);
+void graphe_ecrit_nX(Graphe g, unsigned int nX);
+void graphe_ecrit_nA(Graphe g, unsigned int nA);
+unsigned int graphe_lit_nX(Graphe g);
+unsigned int graphe_lit_nA(Graphe g);
+
+double graphe_lit_poids(Graphe g, unsigned int u);
+void graphe_ecrit_poids(Grapheg, unsigned int u, double val);
+void graphe_ecrit_poids_arc(Graphe g, unsigned int u, unsigned int v, double val);
+double graphe_lit_poids_arc(Graphe g, unsigned int u, unsigned int v);
+
+Graphe lit_graphe(char* fichier);
+void graphe_ajoute_arc(Graphe g, unsigned int u, unsigned int v, double val);
+double graphe_pcc(Graphe g, unsigned int u, unsigned int v);
+
+#endif // _Graphe
