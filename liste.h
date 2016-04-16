bf9b7fca6e22f514e501db13d781aeaec8bb8338
Sheppifan <caroline.celie34@gmail.com>
2016-04-16 14:05:13 +0200

    Added files via upload

---
+++ liste.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "carte.h"
+
+#ifndef _Liste
+#define _Liste
+
+struct cellule {
+	CARTE val;
+	struct cellule* suiv;};
+
+typedef struct cellule* Liste;
+
+Liste creer_liste(void);
+int est_vide(Liste l);
+void visualiser_liste(Liste l);
+Liste ajout_tete(CARTE c,Liste l);
+Liste supprimer_tete(Liste l);
+Liste ajout_queue(CARTE c,Liste l);
+Liste supprimen(int n,Liste l);
+
+#endif
+
