175f960bb0cfe3f5c6f23f293b71af52ce2a1974
Sheppifan <caroline.celie34@gmail.com>
2016-04-26 23:11:43 +0200

    Added files via upload

--- graphe.c
+++ graphe.c
@@ -26,14 +26,62 @@ unsigned int graphe_lit_nA(Graphe g)*/
 
 ///Fonctions de Caroline
 
-/*double graphe_lit_poids(Graphe g, unsigned int u)
+///Revoir les tests de securite
 
-void graphe_ecrit_poids(Grapheg, unsigned int u, double val)
+double graphe_lit_poids(Graphe g, unsigned int u)       //lit le poids du noeud u du graphe g
+{
+    Graphe p=g;
+    double poids_noeud;
+   //int i=0;
+    if(g==NULL) { printf("graphe vide\n"); return 0;}
+    /*for(i=0;i<=u;i++)
+    {   if(p+1==NULL) { printf("graphe vide\n"); return 0;}
+        else p=p+1;
+    }*/
+    p=p+u;
+    poids_noeud=p->poids_noeud;
+    return poids_noeud;
+}
 
-void graphe_ecrit_poids_arc(Graphe g, unsigned int u, unsigned int v, double val)
+void graphe_ecrit_poids(Graphe g, unsigned int u, double valeur) //ecrit dans le champ poids_noeud le poids du noeud u du graphe g
+{
+    //Graphe p=g;
+    if(g==NULL) printf("graphe vide\n");
+    (g+u)->poids_noeud=valeur;
 
-double graphe_lit_poids_arc(Graphe g, unsigned int u, unsigned int v)*/
+}
 
+void graphe_ecrit_poids_arc(Graphe g, unsigned int u, unsigned int v, double valeur)///ameliorer les tests
+{
+    int i=0;
+    Graphe p=g;
+    Liste l=creer_liste();
+    if(g==NULL) printf("graphe vide\n");
+    p=g+u;
+    l=p->arc;
+    for(i=0;i<=v;i++)
+    {
+        l=l->suiv;
+    }
+    l->val.poids_arc=valeur;
+}
+
+double graphe_lit_poids_arc(Graphe g, unsigned int u, unsigned int v)
+{
+    double valeur;
+    int i=0;
+    Graphe p=g;
+    Liste l=creer_liste();
+    if(g==NULL) printf("graphe vide\n");
+    p=g+u;
+    l=p->arc;
+    for(i=0;i<=v;i++)
+    {
+        l=l->suiv;
+    }
+    valeur=l->val.poids_arc;
+    return valeur;
+}
 
 ///fonctions Haut niveau
 
