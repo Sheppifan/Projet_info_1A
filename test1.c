338e2dc8fa07b046dcca3c4b3e16d418fb78fb1a
guillau4 <laure.guill78@gmail.com>
2016-05-11 15:56:03 +0200

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,22 @@
+#include graphe.c
+#include liste.c
+#include <stdio.h>
+
+int main()
+{
+	printf("Test 1\n");
+	printf("Donnez nX et nA\n");
+	scanf("%u %u" nX, nA);
+	getchar();
+	Graphe g = nouveau_graphe( nX, nA);
+	int i;
+	sommet* tableau=Graphe->stations
+	for (i=0; i<graphe_lit_nX(Graphe g); i++)
+	{
+		printf("Donnez le nom de station, ligne, numero ligne, poids et destination")
+		
+	}
+
+
+
+}
