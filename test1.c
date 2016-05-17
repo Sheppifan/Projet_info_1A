f05cd2ae5a88512090c8dea155d0533a41400d0e
guillau4 <laure.guill78@gmail.com>
2016-05-17 18:05:29 +0200

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,30 +1,32 @@
-#include "graphe.c"
-#include "liste.c"
+#include "graphe.h"
+#include "liste.h"
 #include <stdio.h>
 
 int main()
 {
 	printf("Test 1\n");
-	printf("On prends nX et nA = 1\n");
+	printf("On prends nX et nA = 2\n");
 	unsigned int  nX,nA;
-	unsigned char nom;
+	unsigned char nom[50];
 	nX=2;
 	nA=2;
-	Graphe g = nouveau_graphe( nX, nA);
+	Graphe g = nouveau_graphe(nX, nA);
 	unsigned int i;
 	sommet tableau=g->stations;
 	for (i=0; i<graphe_lit_nX(g); i++)
 	{
 		printf("Donnez le nom de station %i\n", i);
-		scanf("%u", &nom);
-		tableau[i].nom_station=nom;
+		scanf("%s", &nom);
+		strcpy(tableau[i].nom_station, nom);
 		getchar();
 	}
+
+
 	printf("Affichage du graphe:\n" );
 	affiche_graphe(g);
 	detruit_graphe(g);
 	printf("graphe detruit\n test affiche");
 	affiche_graphe(g);
 
+    return 0;
 }
-
