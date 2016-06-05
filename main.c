b77800f713aed90924c060d4dae8ee60c9e2f539
Sheppifan <caroline.celie34@gmail.com>
2016-06-05 18:44:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,59 +5,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-charger le fichier.csv et creer le graphe
-
-demande station départ
-
-demande station arrivee
-
-belleman et plus court chemin
-
-affichage information dans pcc
-	trajet
-	temps de trajet
-	
-
-voir affichage
-voir retour sur case
-	nouveau trajet
-	terminee
-	
-
-
-*/
 
 int main()
 {	Graphe g;
-	//unsigned char depart[50];
-	//unsigned char arrivee[50];
-	unsigned int depart;
+	unsigned  int depart;
 	unsigned int arrivee;
-	unsigned int condition;
-	unsigned int continuer=1;
-	
+	char cond;
+
 	char* fichier="metro.csv";
+
+    printf("Bienvenu dans l'interface d'itineraire\n");
 	g=lit_graphe(fichier);
-	
-	while(continuer==1){
-		printf("1 : nouveau trajet\n");
-		printf("0 : quitter\n");
-		scanf("u\n",&condition);
-		switch(condition)
-		{	case 1 :
-				printf("entrez la station de depart\n");
-				scanf("%u\n",&depart);
-				printf("entrez la station d'arrivee\n");
-				scanf("%u\n",&arrivee);
-				pcc(g,depart,arrivee);
-				continuer=1;
-				break;
-			
-			case 0 :
-				continuer=0;
-				break;
-		}
-	}
-	return(1);
+	while(cond!='0')
+    {
+        printf("1:nouveau trajet\n");
+        printf("0: quitter\n");
+        fflush(stdout);
+        cond=getchar();
+        switch(cond){
+    case '1' :
+            printf("nouveau trajet\n");
+
+            printf("entrez la station de depart\n");
+            scanf("%u\n",&depart);
+            printf("entrez la station d'arrivee\n");
+            scanf("%u\n",&arrivee);
+
+            pcc(g, depart, arrivee)
+            break;
+
+    case '0':
+            printf("quitter\n");
+            break;
+        }
+        getchar();
+    }
+	return 1;
 }
+
