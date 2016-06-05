53e1b5b10ba09065262103c4144b03d3518c7778
guillau4 <laure.guill78@gmail.com>
2016-06-05 19:58:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include "graphe.h"
 #include "element.h"
 #include "liste.h"
@@ -8,34 +9,34 @@
 
 int main()
 {	Graphe g;
-	unsigned  int depart;
+	unsigned int depart;
 	unsigned int arrivee;
 	char cond;
 
 	char* fichier="metro.csv";
 
-    printf("Bienvenu dans l'interface d'itineraire\n");
+    printf("Bienvenue dans l'interface d'itineraire\n");
 	g=lit_graphe(fichier);
 	while(cond!='0')
     {
-        printf("1:nouveau trajet\n");
-        printf("0: quitter\n");
+        printf("1: Nouveau trajet\n");
+        printf("0: Quitter\n");
         fflush(stdout);
         cond=getchar();
         switch(cond){
     case '1' :
             printf("nouveau trajet\n");
 
-            printf("entrez la station de depart\n");
-            scanf("%u\n",&depart);
-            printf("entrez la station d'arrivee\n");
-            scanf("%u\n",&arrivee);
+            printf("Entrez la station de depart\n");
+            scanf("%u",&depart);
+            printf("Entrez la station d'arrivee\n");
+            scanf("%u",&arrivee);
 
-            pcc(g, depart, arrivee)
+            graphe_pcc(g, depart, arrivee);
             break;
 
     case '0':
-            printf("quitter\n");
+            printf("Quitter\n");
             break;
         }
         getchar();
